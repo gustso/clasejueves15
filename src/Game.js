d9b454ad075e536c443db441debad77e2342650f
gustso <gustso@fi.unju.edu.ar>
2022-09-15 07:09:05 -0700

    Listo el repositorio para trabajar. Gustavo Sosa

---
+++ src/Game.js
@@ -0,0 +1,8 @@
+export default function Game() {
+    return (
+      <h1>Hola MUNDO</h1>
+    );
+  }
+  
+  //export default Game;
+  
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Game from './Game';
+
+const gustso = ReactDOM.createRoot(document.getElementById('gustso'));
+
+gustso.render(    
+  <Game></Game>
+);
